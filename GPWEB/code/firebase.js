77c0e319ddca53521410d541ad36727df7b75edf
Alexandre Rodrigues <alexandre.rodrigues.chagas.silva@gmail.com>
2021-01-13 18:24:42 -0300

    última atualização

--- GPWEB/code/firebase.js
+++ GPWEB/code/firebase.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAfT89FALIcQxpo9rNkk-822I_I2W-btVk",
-    authDomain: "sistemagenerenciadordepedidos.firebaseapp.com",
-    databaseURL: "https://sistemagenerenciadordepedidos.firebaseio.com",
-    projectId: "sistemagenerenciadordepedidos",
-    storageBucket: "sistemagenerenciadordepedidos.appspot.com",
-    messagingSenderId: "898413701429",
-    appId: "1:898413701429:web:91645020ca9a7cac10447c",
-    measurementId: "G-BT4CD70LXL"
+  apiKey: "AIzaSyBoagR3qP7d0vb3Q6pXS-7MjDvhhhjt1dk",
+  authDomain: "chamadordepedidos.firebaseapp.com",
+  databaseURL: "https://chamadordepedidos.firebaseio.com",
+  projectId: "chamadordepedidos",
+  storageBucket: "chamadordepedidos.appspot.com",
+  messagingSenderId: "173510588155",
+  appId: "1:173510588155:web:bfd00db073ec923174a25e",
+  measurementId: "G-BX3GWYR6W1"
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAfT89FALIcQxpo9rNkk-822I_I2W-btVk",
-    authDomain: "sistemagenerenciadordepedidos.firebaseapp.com",
-    databaseURL: "https://sistemagenerenciadordepedidos.firebaseio.com",
-    projectId: "sistemagenerenciadordepedidos",
-    storageBucket: "sistemagenerenciadordepedidos.appspot.com",
-    messagingSenderId: "898413701429",
-    appId: "1:898413701429:web:91645020ca9a7cac10447c",
-    measurementId: "G-BT4CD70LXL"
+    apiKey: "AIzaSyBoagR3qP7d0vb3Q6pXS-7MjDvhhhjt1dk",
+    authDomain: "chamadordepedidos.firebaseapp.com",
+    databaseURL: "https://chamadordepedidos.firebaseio.com",
+    projectId: "chamadordepedidos",
+    storageBucket: "chamadordepedidos.appspot.com",
+    messagingSenderId: "173510588155",
+    appId: "1:173510588155:web:bfd00db073ec923174a25e",
+    measurementId: "G-BX3GWYR6W1"
   }
 };
